8590592e5be6d193900925ee1d59f882752fee9c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-16 20:03:57 -0700

    Defer Blocks

--- src/app/dashboard/pages/defer-views/defer-views.component.ts
+++ src/app/dashboard/pages/defer-views/defer-views.component.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders-slow/heavy-loaders-slow.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeavyLoadersSlowComponent],
   templateUrl: './defer-views.component.html',
-  styleUrl: './defer-views.component.css'
+  styleUrl: './defer-views.component.css',
 })
-export default class DeferViewsComponent {
-
-}
+export default class DeferViewsComponent {}
