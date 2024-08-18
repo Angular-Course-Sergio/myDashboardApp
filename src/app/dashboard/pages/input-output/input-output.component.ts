51282285d6382b22e6babeb9934ecf448607600a
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-17 23:00:35 -0700

    Preparar página para Input y Output

--- src/app/dashboard/pages/input-output/input-output.component.ts
+++ src/app/dashboard/pages/input-output/input-output.component.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ProductCardComponent } from '@dashboardPages/ui/product-card/product-card.component';
 import { TitleComponent } from '@shared/title/title.component';
+import { ProductCardComponent } from './ui/product-card/product-card.component';
 
 @Component({
-  selector: 'app-product-card',
   standalone: true,
   imports: [CommonModule, TitleComponent, ProductCardComponent],
   templateUrl: './input-output.component.html',
--- src/app/dashboard/pages/input-output/ui/product-card/product-card.component.ts
+++ src/app/dashboard/pages/input-output/ui/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-product-card',
   standalone: true,
   imports: [],
   templateUrl: './product-card.component.html',
