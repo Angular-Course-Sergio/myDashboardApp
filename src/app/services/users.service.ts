2bd45b32bbcdc7b089fd3703b6dcc2b23fe5e166
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-17 18:01:23 -0700

    Peticiones HTTP - importProvidersFrom

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,16 +1,32 @@
-import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { ReqResponse } from '@interfaces/req-response.interface';
 import { State } from '@interfaces/state.interface';
+import { delay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
+  private http = inject(HttpClient);
+
   #state = signal<State>({
     loading: true,
     users: [],
   });
 
+  public users = computed(() => this.#state().users);
+  public loading = computed(() => this.#state().loading);
+
   constructor() {
-    console.log('Cargando data');
+    this.http
+      .get<ReqResponse>('https://reqres.in/api/users')
+      .pipe(delay(1500))
+      .subscribe((res) => {
+        this.#state.set({
+          loading: false,
+          users: res.data,
+        });
+      });
   }
 }
