07c0cd5acdb9633fa4c35a734d054f094c7f3cec
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-17 23:10:47 -0700

    Añadir productos dinámicamente

--- src/app/dashboard/pages/input-output/input-output.component.ts
+++ src/app/dashboard/pages/input-output/input-output.component.ts
@@ -1,7 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  signal,
+} from '@angular/core';
 import { TitleComponent } from '@shared/title/title.component';
 import { ProductCardComponent } from './ui/product-card/product-card.component';
+import { Product } from '@interfaces/product.interface';
+import { interval, take, tap } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -10,4 +17,31 @@ import { ProductCardComponent } from './ui/product-card/product-card.component';
   styleUrl: './input-output.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class InputOutputComponent {}
+export default class InputOutputComponent implements OnDestroy {
+  public products = signal<Product[]>([
+    { id: 1, name: 'Producto 1', quantity: 1 },
+    { id: 2, name: 'Producto 2', quantity: 1 },
+    { id: 3, name: 'Producto 3', quantity: 1 },
+    { id: 4, name: 'Producto 4', quantity: 1 },
+  ]);
+
+  private intervalSuscription = interval(1000)
+    .pipe(
+      tap(() => {
+        this.products.update((products) => [
+          ...products,
+          {
+            id: products.length + 1,
+            name: `Producto ${products.length + 1}`,
+            quantity: 1,
+          },
+        ]);
+      }),
+      take(7)
+    )
+    .subscribe();
+
+  ngOnDestroy(): void {
+    this.intervalSuscription.unsubscribe();
+  }
+}
